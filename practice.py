a7d3db182a9d710868fd982594713e5761998ec1
Cynthia Ordaz <cordaz@dons.usfca.edu>
2020-01-15 23:55:33 -0800

    Update practice.py

--- practice.py
+++ practice.py
@@ -1,2 +1,14 @@
-letters = ('c','b','d')
-new_letters = ('a',) + letters
+text = " "
+
+while text != "quit":
+   text = input("Please enter a channel formula (or 'quit' to exit): ")
+   if text == "quit":
+      print(" exting prgram")
+   elif text == "H2O"
+      print("Water")
+    elif text =="NH3"
+      print("Ammonia")
+    elif text == "CH4":
+      print("Methane")
+    else:
+      print("Uknown Compound")
